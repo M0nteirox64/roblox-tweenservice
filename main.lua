1247c9302f17cca500fa19030ba4eaf0cee5f17e
monteiro, pt <trxshroot@gmail.com>
2025-03-13 00:47:19 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -4,8 +4,8 @@ local TS = game:GetService("TweenService")
 local Part = game.Workspace.Mov
 
 proximityPrompt.Triggered:Connect(function(player)
-	local TI = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, true, 0)
+	local TI = TweenInfo.new(2, Enum.EasingStyle.Linear, Enum.EasingDirection.Out, 0, false, 0)
 	print("[ + ] O jogador " .. player.Name .. " interagiu com a part")
 	local tween = TS:Create(game.Workspace.Mov,TI, {CFrame = CFrame.new(12, 8.5, 32)}):Play()
-	print("[ + ] Nova posição da part: ".. Part.Position)
+	print("[ + ] Posição da part: ".. tostring(Part.Position))
 end)
